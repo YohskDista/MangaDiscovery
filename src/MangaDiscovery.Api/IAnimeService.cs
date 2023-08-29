ac553f54577f416751c694f4f64cf279e484055e
YohskDiSta <yohsk10@gmail.com>
2023-08-29 21:26:34 +0200

    Create the project and start service implementation

---
+++ src/MangaDiscovery.Api/IAnimeService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MangaDiscovery.Api
+{
+    public interface IAnimeService
+    {
+        Task Search(string searchedTitle);
+    }
+
+    internal class CrunchyrollAnimeService : IAnimeService
+    {
+        private readonly ILogger<CrunchyrollAnimeService> _logger;
+        private readonly HttpClient _httpClient;
+
+        public CrunchyrollAnimeService(ILogger<CrunchyrollAnimeService> logger, HttpClient httpClient)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        /// <inheritdoc />
+        public Task Search(string searchedTitle)
+        {
+            throw new NotImplementedException();
+        }
+
+        private record SearchResult (int CurrentPage, bool HasNextPage, IEnumerable<MangaResult> results);
+
+        private record MangaResult (string Id, string Title, string Image, string Cover, bool HasDub, bool HasSub, IEnumerable<string> Genres);
+    }
+}
---
+++ src/MangaDiscovery.Api/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MangaDiscovery.Api
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAnimeServices(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.AddSingleton<IAnimeService, CrunchyrollAnimeService>();
+        }
+    }
+}
